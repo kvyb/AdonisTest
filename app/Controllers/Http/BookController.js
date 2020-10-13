4b2cb7f8a6d097ee4bfaa0b9dba1c926b82b8ff2
kvyb <vybiral.kristian@gmail.com>
2020-10-13 19:27:55 +0300

    feat form and initial setup

---
+++ app/Controllers/Http/BookController.js
@@ -0,0 +1,51 @@
+'use strict'
+//import model to be able to use it for handling form input via controller to DB
+const Book = use('App/Models/Book')
+const { validate } = use("Validator")
+
+class BookController {
+    async index({ view }) {
+      const books = [
+        { title: "Book 1", author: "John Doe" },
+        { title: "Book 2", author: "Jane Doe" },
+        { title: "Book 3", author: "Adam Smith" },
+      ];
+  
+      return view.render("book.index", {
+        books: books,
+      });
+    }
+    //creates form referred from router and renders /book/create view
+    async create({ view }) {
+        return view.render("book.create");
+    }
+    //method to store book information from above created view input in DB
+    async store({ request, response, session }) {
+        const validation = await validate(request.all(), {
+            title: "required",
+            author: "required",
+            cover_image: "required",
+            isbn: "required|min:10|max:10",
+        });
+
+        if (validation.fails()) {
+            session.withErrors(validation.messages()).flashAll();
+            return response.redirect("back");
+        }
+
+        const book = new Book();
+    
+        book.title = request.input("title");
+        book.author = request.input("author");
+        book.cover_image = request.input("cover_image");
+        book.isbn = request.input("isbn");
+    
+        await book.save();
+
+        session.flash({ notification: "Book has been Created" });
+        return response.redirect("/");
+      }
+
+  }
+
+module.exports = BookController
